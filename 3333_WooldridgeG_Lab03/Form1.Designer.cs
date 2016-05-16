581a2dd21873c5232929a8fc21989fdf01690c31
gagewooldridge@gmail.com <gagewooldridge@gmail.com>
2016-05-16 17:15:08 -0500

    Stadium seating lab
    This program will take user input values that represent the amount of
    tickets sold at very price points, and calculate the total of all ticket
    sales as well as keep a runningmgrand total of all data input in the
    program

---
+++ 3333_WooldridgeG_Lab03/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace _3333_WooldridgeG_Lab03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.classATextBox = new System.Windows.Forms.TextBox();
+            this.classBTextBox = new System.Windows.Forms.TextBox();
+            this.classCTextBox = new System.Windows.Forms.TextBox();
+            this.ticketsSoldLabel = new System.Windows.Forms.Label();
+            this.userPromptingLabel = new System.Windows.Forms.Label();
+            this.classALabel = new System.Windows.Forms.Label();
+            this.classBLabel = new System.Windows.Forms.Label();
+            this.classCLabel = new System.Windows.Forms.Label();
+            this.revenueGeneratedLabel = new System.Windows.Forms.Label();
+            this.classARevenueLabel = new System.Windows.Forms.Label();
+            this.classBRevenueLabel = new System.Windows.Forms.Label();
+            this.classCRevenueLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.outputALabel = new System.Windows.Forms.Label();
+            this.outputBLabel = new System.Windows.Forms.Label();
+            this.outputCLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalRevenueLabel = new System.Windows.Forms.Label();
+            this.overallRevenueLabel = new System.Windows.Forms.Label();
+            this.runningTotalLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // classATextBox
+            // 
+            resources.ApplyResources(this.classATextBox, "classATextBox");
+            this.classATextBox.Name = "classATextBox";
+            // 
+            // classBTextBox
+            // 
+            resources.ApplyResources(this.classBTextBox, "classBTextBox");
+            this.classBTextBox.Name = "classBTextBox";
+            // 
+            // classCTextBox
+            // 
+            resources.ApplyResources(this.classCTextBox, "classCTextBox");
+            this.classCTextBox.Name = "classCTextBox";
+            // 
+            // ticketsSoldLabel
+            // 
+            resources.ApplyResources(this.ticketsSoldLabel, "ticketsSoldLabel");
+            this.ticketsSoldLabel.Name = "ticketsSoldLabel";
+            // 
+            // userPromptingLabel
+            // 
+            resources.ApplyResources(this.userPromptingLabel, "userPromptingLabel");
+            this.userPromptingLabel.Name = "userPromptingLabel";
+            // 
+            // classALabel
+            // 
+            resources.ApplyResources(this.classALabel, "classALabel");
+            this.classALabel.Name = "classALabel";
+            // 
+            // classBLabel
+            // 
+            resources.ApplyResources(this.classBLabel, "classBLabel");
+            this.classBLabel.Name = "classBLabel";
+            // 
+            // classCLabel
+            // 
+            resources.ApplyResources(this.classCLabel, "classCLabel");
+            this.classCLabel.Name = "classCLabel";
+            // 
+            // revenueGeneratedLabel
+            // 
+            resources.ApplyResources(this.revenueGeneratedLabel, "revenueGeneratedLabel");
+            this.revenueGeneratedLabel.Name = "revenueGeneratedLabel";
+            // 
+            // classARevenueLabel
+            // 
+            resources.ApplyResources(this.classARevenueLabel, "classARevenueLabel");
+            this.classARevenueLabel.Name = "classARevenueLabel";
+            // 
+            // classBRevenueLabel
+            // 
+            resources.ApplyResources(this.classBRevenueLabel, "classBRevenueLabel");
+            this.classBRevenueLabel.Name = "classBRevenueLabel";
+            // 
+            // classCRevenueLabel
+            // 
+            resources.ApplyResources(this.classCRevenueLabel, "classCRevenueLabel");
+            this.classCRevenueLabel.Name = "classCRevenueLabel";
+            // 
+            // calculateButton
+            // 
+            resources.ApplyResources(this.calculateButton, "calculateButton");
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            resources.ApplyResources(this.clearButton, "clearButton");
+            this.clearButton.Name = "clearButton";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            resources.ApplyResources(this.exitButton, "exitButton");
+            this.exitButton.Name = "exitButton";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // outputALabel
+            // 
+            this.outputALabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            resources.ApplyResources(this.outputALabel, "outputALabel");
+            this.outputALabel.Name = "outputALabel";
+            // 
+            // outputBLabel
+            // 
+            this.outputBLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            resources.ApplyResources(this.outputBLabel, "outputBLabel");
+            this.outputBLabel.Name = "outputBLabel";
+            // 
+            // outputCLabel
+            // 
+            this.outputCLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            resources.ApplyResources(this.outputCLabel, "outputCLabel");
+            this.outputCLabel.Name = "outputCLabel";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            resources.ApplyResources(this.totalLabel, "totalLabel");
+            this.totalLabel.Name = "totalLabel";
+            // 
+            // totalRevenueLabel
+            // 
+            resources.ApplyResources(this.totalRevenueLabel, "totalRevenueLabel");
+            this.totalRevenueLabel.Name = "totalRevenueLabel";
+            // 
+            // overallRevenueLabel
+            // 
+            resources.ApplyResources(this.overallRevenueLabel, "overallRevenueLabel");
+            this.overallRevenueLabel.Name = "overallRevenueLabel";
+            // 
+            // runningTotalLabel
+            // 
+            this.runningTotalLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            resources.ApplyResources(this.runningTotalLabel, "runningTotalLabel");
+            this.runningTotalLabel.Name = "runningTotalLabel";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.calculateButton;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.clearButton;
+            this.Controls.Add(this.runningTotalLabel);
+            this.Controls.Add(this.overallRevenueLabel);
+            this.Controls.Add(this.totalRevenueLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.outputCLabel);
+            this.Controls.Add(this.outputBLabel);
+            this.Controls.Add(this.outputALabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.classCRevenueLabel);
+            this.Controls.Add(this.classBRevenueLabel);
+            this.Controls.Add(this.classARevenueLabel);
+            this.Controls.Add(this.revenueGeneratedLabel);
+            this.Controls.Add(this.classCLabel);
+            this.Controls.Add(this.classBLabel);
+            this.Controls.Add(this.classALabel);
+            this.Controls.Add(this.userPromptingLabel);
+            this.Controls.Add(this.ticketsSoldLabel);
+            this.Controls.Add(this.classCTextBox);
+            this.Controls.Add(this.classBTextBox);
+            this.Controls.Add(this.classATextBox);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox classATextBox;
+        private System.Windows.Forms.TextBox classBTextBox;
+        private System.Windows.Forms.TextBox classCTextBox;
+        private System.Windows.Forms.Label ticketsSoldLabel;
+        private System.Windows.Forms.Label userPromptingLabel;
+        private System.Windows.Forms.Label classALabel;
+        private System.Windows.Forms.Label classBLabel;
+        private System.Windows.Forms.Label classCLabel;
+        private System.Windows.Forms.Label revenueGeneratedLabel;
+        private System.Windows.Forms.Label classARevenueLabel;
+        private System.Windows.Forms.Label classBRevenueLabel;
+        private System.Windows.Forms.Label classCRevenueLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label outputALabel;
+        private System.Windows.Forms.Label outputBLabel;
+        private System.Windows.Forms.Label outputCLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label totalRevenueLabel;
+        private System.Windows.Forms.Label overallRevenueLabel;
+        private System.Windows.Forms.Label runningTotalLabel;
+
+    }
+}
+
